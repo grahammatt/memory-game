c569ec1ea62431e4eefb2d940ec455f386a3aa2d
Matt Graham <grahammatt@live.com>
2018-05-22 18:24:40 -0500

    bugfix: fixed bug with clicking already matched cards

--- scripts/main.js
+++ scripts/main.js
@@ -4,20 +4,30 @@ const CARD_TABLE = document.getElementById("card-table");
 // TODO: Change CARD_AMOUNT to let and make the grid creation allow for larger games.
 const CARD_AMOUNT = 16;
 let last_click = false;
+let matches = 0;
+let turns = 0;
 const ICONS = ['fas fa-basketball-ball pic', 'fas fa-bicycle pic', 'fas fa-bomb pic', 'fas fa-bolt pic', 'fas fa-frog pic', 'fas fa-football-ball pic', 'fas fa-beer pic', 'fas fa-dice pic'];
 
 function getRandomInt(min, max) {
   //from MDN Documentation
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
+function matchCount(){
+  matches++;
+  if (matches ===CARD_AMOUNT/2){
+    while (CARD_TABLE.firstChild) {
+      CARD_TABLE.removeChild(CARD_TABLE.lastChild);
+    }
+  }
+}
 function game(event) {
   let card = event.target;
-  if (card.classList.contains('card-table')) {
-    return 0;
-  } else if (card.classList.contains('pic')) {
+  if (card.classList.contains('pic')) {
     card = card.parentElement;
   }
+  if (card.classList.contains("card-table") || card.classList.contains("match")) {
+    return 0;
+  } 
   card.firstElementChild.style.display = 'initial';
   card.style.background = '#F5F5F5';
   if (card.classList.contains('card') && card != last_click) {
@@ -27,17 +37,20 @@ function game(event) {
       return 0;
     }
     last_click.firstElementChild.classList.remove('animated', 'rubberBand');
+    CARD_TABLE.removeEventListener("click", game);
     if (card.firstElementChild.className == last_click.firstElementChild.className) {
 
       card.classList.add('match', 'animated', 'tada');
       last_click.classList.add('match', 'animated', 'tada');
+     // match_count();
       last_click = false;
-    } else {
+    } 
+    else {
       card.classList.add('mismatch', 'animated', 'shake');
       last_click.classList.add('mismatch', 'animated', 'shake');
 
-      CARD_TABLE.removeEventListener('click', game);
-      setTimeout(function() {
+      
+      setTimeout(function () {
         card.firstElementChild.style.display = 'none';
         card.style.background = '#118ab2';
         last_click.firstElementChild.style.display = 'none';
@@ -45,9 +58,11 @@ function game(event) {
         card.classList.remove('mismatch', 'animated', 'shake');
         last_click.classList.remove('mismatch', 'animated', 'shake');
         last_click = false;
-        CARD_TABLE.addEventListener('click', game);
       }, 1000);
     }
+    setTimeout(function () {
+      CARD_TABLE.addEventListener('click', game);
+    }, 1000);
   }
 
 
