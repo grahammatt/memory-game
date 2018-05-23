db7b551ba7ba817c7345c0a8fd35cb2c34b1184b
Matt Graham <grahammatt@live.com>
2018-05-23 00:50:53 -0500

    feat: implement modal for win condition and reset button

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,7 @@
 const CARD_TABLE = document.getElementById("card-table");
 // TODO: Change CARD_AMOUNT to let and make the grid creation allow for larger games.
 const CARD_AMOUNT = 16;
+const MODAL = document.getElementById("win-modal");
 let last_click = false;
 let matches = 0;
 let turns = 0;
@@ -12,14 +13,15 @@ function getRandomInt(min, max) {
   //from MDN Documentation
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-function matchCount(){
-  matches++;
-  if (matches ===CARD_AMOUNT/2){
-    while (CARD_TABLE.firstChild) {
-      CARD_TABLE.removeChild(CARD_TABLE.lastChild);
-    }
+
+function matchCount() {
+  if (++matches === CARD_AMOUNT / 2) {
+    matches = 0;
+    MODAL.style.display = 'flex';
+
   }
 }
+
 function game(event) {
   let card = event.target;
   if (card.classList.contains('pic')) {
@@ -27,7 +29,7 @@ function game(event) {
   }
   if (card.classList.contains("card-table") || card.classList.contains("match")) {
     return 0;
-  } 
+  }
   card.firstElementChild.style.display = 'initial';
   card.style.background = '#F5F5F5';
   if (card.classList.contains('card') && card != last_click) {
@@ -42,15 +44,14 @@ function game(event) {
 
       card.classList.add('match', 'animated', 'tada');
       last_click.classList.add('match', 'animated', 'tada');
-     // match_count();
+      matchCount();
       last_click = false;
-    } 
-    else {
+    } else {
       card.classList.add('mismatch', 'animated', 'shake');
       last_click.classList.add('mismatch', 'animated', 'shake');
 
-      
-      setTimeout(function () {
+
+      setTimeout(function() {
         card.firstElementChild.style.display = 'none';
         card.style.background = '#118ab2';
         last_click.firstElementChild.style.display = 'none';
@@ -60,7 +61,7 @@ function game(event) {
         last_click = false;
       }, 1000);
     }
-    setTimeout(function () {
+    setTimeout(function() {
       CARD_TABLE.addEventListener('click', game);
     }, 1000);
   }
@@ -85,5 +86,12 @@ function makeGrid() {
   }
   CARD_TABLE.append(fragment); //add the cards to the grid layout
 }
+document.getElementById('reset-game').addEventListener("click", function() {
+  while (CARD_TABLE.firstChild) {
+    CARD_TABLE.removeChild(CARD_TABLE.lastChild);
+  }
+  MODAL.style.display = 'none';
+  makeGrid();
+});
 makeGrid();
 CARD_TABLE.addEventListener('click', game);
